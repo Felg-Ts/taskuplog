b7a4598bf6c2c3fad766ed446cc0cb55a13d86e6
Felg-Ts <78023510+Felg-Ts@users.noreply.github.com>
2023-01-08 16:35:26 +0100

    Cambio formato de fecha

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def insertar():
 @app.route('/query', methods=['GET'])
 def query():
   # Crear consulta de inserción
-  query = "select servidor,plugin,version,fecha,CASE obsoleto WHEN 1 THEN 'Sí' ELSE 'No' END AS obsoleto,CASE premium WHEN 1 THEN 'Sí' ELSE 'No' END AS premium,CASE db WHEN 1 THEN 'Sí' ELSE 'No' END AS db from registro"
+  query = "select servidor,plugin,version,date_format(fecha, '%d-%m-%Y') AS fecha,CASE obsoleto WHEN 1 THEN 'Sí' ELSE 'No' END AS obsoleto,CASE premium WHEN 1 THEN 'Sí' ELSE 'No' END AS premium,CASE db WHEN 1 THEN 'Sí' ELSE 'No' END AS db from registro"
 
   # Conectarse a la base de datos y ejecutar consulta
   conn = mysql.connector.connect(host='192.168.50.28', user='taskuploguser', password='taskuplogpass', database='taskuplogdb')
